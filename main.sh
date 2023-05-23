21aa686a8ad58d70b23ca9cd258f247d4a184723
Kazarin Fyodor <fekazar@gmail.com>
2023-05-23 23:16:32 +0300

    Added config for docker ans sublime text

--- main.sh
+++ main.sh
@@ -1,8 +1,31 @@
 #/bin/bash
+
+if [ $(id -u) -ne 0 ]
+  then echo 'Canceled. Should be ran as root.'
+  exit
+fi
+
 echo '[SYSTEM CONFIGURATION STARTED]'
 
-sudo apt update
+echo '[PHASE BASE INIT]'
 
+chmod +x *.init
+chmod +x *.install
+
+apt update
+apt install -y wget \
+  gpg \
+  vim \
+  curl
+
+echo '[PHASE INIT]'
+for f in *.init; do
+  bash "$f"
+done
+
+apt update
+
+echo '[PHASE INSTALL]'
 for f in *.install; do
   bash "$f"
 done
