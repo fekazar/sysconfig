19db9684f4d96a4ba744d0987828696b37c836bf
Fyodor Kazarin <fekazar@gmail.com>
2023-10-29 11:24:38 +0300

    Added nvim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,30 @@
+-- Set vim properties
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.showcmd = true
+
+vim.g.mapleader = " "
+
+-- Install Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ import = "plugins" })
+require('nvim-ts-autotag').setup()
+
+
---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return { 
+  "ellisonleao/gruvbox.nvim", 
+  priority = 1000 , 
+  -- config = true, 
+  config = function()
+    vim.cmd([[colorscheme gruvbox]])
+  end,
+  opts = ...
+}
