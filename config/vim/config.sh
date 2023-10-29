924d364f921c7ce797bfff6abc67fef40dd8a2c0
Fyodor Kazarin <fekazar@gmail.com>
2023-10-29 11:41:55 +0300

    Autodelete git directories

--- config/vim/config.sh
+++ config/vim/config.sh
@@ -10,10 +10,14 @@ if [ ! -f ~/.vimrc ]; then
 fi
 
 git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+rm -rf ~/.vim/pack/dist/start/vim-airline/.git
 
 echo ':helptags ~/.vim/pack/dist/start/vim-airline/doc'
 
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+rm -rf ~/.vim/pack/vendor/start/nerdtree/.git
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+rm -rf ~/.vim/pack/default/start/gruvbox/.git
+
